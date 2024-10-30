b5aea0d56c0b18100656e71348b376e2f1d08756
huutuan783 <tuanhuu783@gmail.com>
2024-10-30 20:48:27 +0700

    add giao dien dang ki

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Screen_01 from "./screens/Screen_01";
 import Screen_02 from "./screens/Screen_02";
+import Screen_03 from "./screens/Screen_03";
 
 const Stack = createStackNavigator();
 
@@ -11,9 +12,10 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Screen_01"screenOptions={{ headerShown: false }} >
+          <Stack.Navigator initialRouteName="Screen_03"screenOptions={{ headerShown: false }} >
             <Stack.Screen name="Screen_01" component={Screen_01} />
             <Stack.Screen name="Screen_02" component={Screen_02} />
+            <Stack.Screen name="Screen_03" component={Screen_03} />
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaView>
