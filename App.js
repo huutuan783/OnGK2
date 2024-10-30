14a11b4528d5567d8e620adc6e6908020afe531e
huutuan783 <tuanhuu783@gmail.com>
2024-10-30 22:54:35 +0700

    fix

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import Screen_01 from "./screens/Screen_01";
-import Screen_02 from "./screens/Screen_02";
-import Screen_03 from "./screens/Screen_03";
+import Screen_dangnhap from "./screens/Screen_dangnhap";
+import Screen_giaodien from "./screens/Screen_giaodien";
+import Screen_dangky from "./screens/Screen_dangky";
 
 const Stack = createStackNavigator();
 
@@ -12,10 +12,10 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Screen_03"screenOptions={{ headerShown: false }} >
-            <Stack.Screen name="Screen_01" component={Screen_01} />
-            <Stack.Screen name="Screen_02" component={Screen_02} />
-            <Stack.Screen name="Screen_03" component={Screen_03} />
+          <Stack.Navigator initialRouteName="Screen_dangky"screenOptions={{ headerShown: false }} >
+            <Stack.Screen name="Screen_dangnhap" component={Screen_dangnhap} />
+            <Stack.Screen name="SScreen_giaodiencreen_02" component={Screen_giaodien} />
+            <Stack.Screen name="Screen_dangky" component={Screen_dangky} />
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaView>
---
+++ screens/Screen_dangnhap.js
@@ -0,0 +1,222 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Modal,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function LoginScreen() {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
+
+  const toggleShowPassword = () => {
+    setPasswordVisible(!isPasswordVisible);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Ionicons
+        name="arrow-back"
+        size={24}
+        color="black"
+        style={styles.backIcon}
+      />
+
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("../assets/Data/icon.png")}
+          style={styles.logo}
+        />
+      </View>
+
+      <Text style={styles.title}>Hello Again!</Text>
+      <Text style={styles.subtitle}>Log into your account</Text>
+
+      <View style={styles.inputContainer}>
+        <Ionicons
+          name="mail-outline"
+          size={20}
+          color="gray"
+          style={styles.icon}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your email address"
+          placeholderTextColor="#aaa"
+          keyboardType="email-address"
+          onChangeText={setEmail}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Ionicons
+          name="lock-closed-outline"
+          size={20}
+          color="gray"
+          style={styles.icon}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your password"
+          placeholderTextColor="#aaa"
+          secureTextEntry={!isPasswordVisible}
+          value={password}
+          onChangeText={setPassword}
+        />
+        <TouchableOpacity onPress={toggleShowPassword}>
+          <Ionicons
+            name={isPasswordVisible ? "eye-off-outline" : "eye-outline"}
+            size={20}
+            color="gray"
+            style={styles.eyeIcon}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.forgotPassword}>Forgot password?</Text>
+      </TouchableOpacity>
+
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity style={styles.continueButton}>
+          <Text style={styles.continueText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.orContainer}>
+        <View style={styles.line} />
+        <Text style={styles.orText}>or</Text>
+        <View style={styles.line} />
+      </View>
+
+      <View style={styles.socialContainer}>
+        <TouchableOpacity style={styles.socialButton}>
+          <Image
+            source={require("../assets/Data/google.png")}
+            style={styles.socialIcon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.socialButton}>
+          <Image
+            source={require("../assets/Data/face.png")}
+            style={styles.socialIcon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.socialButton}>
+          <Image
+            source={require("../assets/Data/apple.png")}
+            style={styles.socialIcon}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+  },
+  backIcon: {
+    position: "absolute",
+    top: 40,
+    left: 20,
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginBottom: 20,
+    marginTop: 40,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "gray",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  inputContainerFocused: {
+    borderColor: "#6C63FF",
+    borderWidth: 1,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  input: {
+    flex: 1,
+    paddingVertical: 10,
+    outlineWidth: 0,
+  },
+  eyeIcon: {
+    marginLeft: 5,
+  },
+  forgotPassword: {
+    color: "#0ad4fa",
+    alignSelf: "flex-end",
+    marginBottom: 20,
+  },
+  continueButton: {
+    backgroundColor: "#0ad4fa",
+    borderRadius: 10,
+    paddingVertical: 15,
+    alignItems: "center",
+  },
+  continueText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  orContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  line: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "gray",
+  },
+  orText: {
+    marginHorizontal: 10,
+    color: "gray",
+  },
+  socialContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  socialButton: {
+    padding: 3,
+  },
+  socialIcon: {
+    width: 50,
+    height: 44,
+  },
+});
